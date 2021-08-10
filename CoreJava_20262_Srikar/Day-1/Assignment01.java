0bff5ee22db153547fd96e3c60040c25e8e5e2f6
Peddapally Srikar <peddapally.srikar@sonata-software.com>
2021-08-10 17:51:51 +0530

    qn 1 solved

---
+++ CoreJava_20262_Srikar/Day-1/Assignment01.java
@@ -0,0 +1,24 @@
+package com.sonata;
+
+public class Assignment01 {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=25;
+		int c=89;
+		if(a>b && a>c)
+		{
+			System.out.println("largest no. is " + a);
+		}
+		else if (b>c)
+		{
+			System.out.println("largest is " + b);
+		}
+		else
+		{
+			System.out.println("largest is "+ c);
+		}
+
+	}
+
+}
