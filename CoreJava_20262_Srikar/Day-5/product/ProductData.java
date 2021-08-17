f11aacfc2cd610b92c8009098c68afb347cbe07f
Srikar Peddapally <75742362+srikarpeddapally@users.noreply.github.com>
2021-08-18 02:09:15 +0530

    Day 5 assignment

---
+++ CoreJava_20262_Srikar/Day-5/product/ProductData.java
@@ -0,0 +1,16 @@
+package com.sonata.product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductData {
+
+	public static List<Products>getProducts(){
+		List<Products> product = new ArrayList();
+		product.add(new Products(1,"oneplus",4000));
+		product.add(new Products(2,"samsung",2000));
+		product.add(new Products(3,"apple",1000));
+		product.add(new Products(4,"nokia",10000));
+		return product;
+	}
+}
